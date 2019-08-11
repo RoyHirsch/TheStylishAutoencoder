aee9df2f10fd3558ad1d21d4b02285d47d1ec9fc
galshachaf <galshac@gmail.com>
2019-08-11 15:29:44 +0300

    src_label should be the true label regardless of transfer
    Signed-off-by: galshachaf <galshac@gmail.com>

--- evaluate.py
+++ evaluate.py
@@ -145,9 +145,9 @@ def test_random_samples(data_iter, model_enc, model_dec, model_cls, device, deco
 
             # logical not on labels if transfer_style is set
             if transfer_style:
+                true_labels = copy.deepcopy(labels)
                 labels = ~labels.byte()
                 labels = labels.long()
-                true_labels = copy.deepcopy(labels)
 
             encode_out = model_enc(src, src_mask)
             preds = model_dec(encode_out, labels, src_mask, src, trg_mask)
