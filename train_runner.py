abc80c3e22dea1c5e64a8d391e20919393fcac74
galshachaf <galshac@gmail.com>
2019-08-11 15:31:27 +0300

    should try to load datasets before recreating them
    Signed-off-by: galshachaf <galshac@gmail.com>

--- train_runner.py
+++ train_runner.py
@@ -20,11 +20,18 @@ from params import *
 # sys.path.
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-imdb_train = imdb_dataset(directory='data', train=True)
-imdb_test = imdb_dataset(directory='data', test=True)
+output_dataset_file = DATA_PATH + "data.pkl"
+try:
+    train_dataset, test_dataset = torch.load(output_dataset_file)
 
-train_dataset = IMDBDataset(imdb_train, tokenizer)
-test_dataset = IMDBDataset(imdb_test, tokenizer)
+except IOError:
+    imdb_train = imdb_dataset(train=True)
+    imdb_test = imdb_dataset(test=True)
+
+    train_dataset = IMDBDataset(imdb_train, tokenizer)
+    test_dataset = IMDBDataset(imdb_test, tokenizer)
+
+    torch.save((train_dataset, test_dataset), output_dataset_file)
 
 print('Train dataset len: {} Test dataset len: {}'.format(len(train_dataset), len(test_dataset)))
 
