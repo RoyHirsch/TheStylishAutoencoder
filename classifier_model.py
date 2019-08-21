f55b5e87d44842beb39fe05ea5b13c64b4453ee9
galshachaf <galshac@gmail.com>
2019-08-21 15:22:56 +0300

    new classifier with no attention
    Signed-off-by: galshachaf <galshac@gmail.com>

--- classifier_model.py
+++ classifier_model.py
@@ -2,13 +2,13 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
-class Descriminator(nn.Module):
+class DescriminatorAttention(nn.Module):
     ''' https://github.com/AnubhavGupta3377/Text-Classification-Models-Pytorch '''
 
     def __init__(self, output_size, hidden_size,
                  embedding_length, drop_rate):
 
-        super(Descriminator, self).__init__()
+        super().__init__()
 
         self.hidden_size = hidden_size
         # Encoder RNN
@@ -64,3 +64,43 @@ class Descriminator(nn.Module):
         final_feature_map = self.dropout(concatenated_vector)  # shape=(batch_size, num_directions * hidden_size)
         final_out = self.fc(final_feature_map)
         return final_out
+
+class Descriminator(nn.Module):
+    ''' https://github.com/AnubhavGupta3377/Text-Classification-Models-Pytorch '''
+
+    def __init__(self, input_size, hidden_size, output_size, drop_rate):
+
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        # Encoder RNN
+        self.lstm = nn.LSTM(input_size=input_size,
+                            hidden_size=hidden_size,
+                            num_layers=1,
+                            bidirectional=True)
+
+        # Dropout Layer
+        self.dropout = nn.Dropout(drop_rate)
+
+        # Fully-Connected Layers
+        self.fc1 = nn.Linear(hidden_size * 2, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+
+    def forward(self, x):
+        x = x.permute(1, 0, 2)
+        # x.shape = (max_sen_len, batch_size, embed_size)
+
+        lstm_output, (h_n, c_n) = self.lstm(x)
+        # lstm_output.shape = (seq_len, batch_size, num_directions * hidden_size)
+
+        # Final hidden state of last layer (num_directions, batch_size, hidden_size)
+        batch_size = h_n.shape[1]
+        final_hidden = h_n.view(2, batch_size, self.hidden_size)
+        cat_final_hidden = torch.cat((final_hidden[0, :, :], final_hidden[1, :, :]), 1)
+
+        out = self.fc1(cat_final_hidden)
+        out = self.dropout(out)
+        out = self.fc2(out)
+
+        return out
